dec9bf75b2eac3234ef8c8c297f2b4f61939d753
Jose Soto <jsoto7087@gmail.com>
2021-05-23 11:28:52 -0400

    commit tree 5335f05253a970b4116f311bbecabc22d6d9107d

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+    // function multiply(num1, num2) {
+
+    let num1 = 31 
+    let num2 = 2
+
+    let multiply = (num1 * num2);
+
+        // console.log(num1 * num2);
+
+    // Math.random()
+
+    let random = Math.floor(Math.random() * 10) + 1;
+
+
+    let num3 = 12 
+    let num4 = 8
+
+    let mod = (num3 % num4);
+
+
+    let max = Math.max(1,2,3,20)
